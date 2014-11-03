b469b1540b827c99c53ee952d431f69e0863ae1c
Konstantin Kamskov <konstantin@kamskov.lt>
2014-11-03 17:25:56 +0000

    providing new field for ebay categories

--- ebay_partner_network.api.php
+++ ebay_partner_network.api.php
@@ -19,8 +19,19 @@ function hook_ebay_partner_network_keyword_alter(&$kw) {
  * Alter the category before performing the search on Ebay.
  */
 function hook_ebay_partner_network_category_alter(&$category) {
-  $menu_object = menu_get_object();
-  if (!empty($menu_object->ebay_category)) {
-    $category = $menu_object->ebay_category;
+  // getting all fields of the 'ebay_category' type
+  $ebay_category_fields = ebay_partner_network_category_fields();
+  if(sizeof($ebay_category_fields) > 0) {
+    //checking if ebay category is set on the node
+    $menu_object = menu_get_object();
+    foreach(array_keys($ebay_category_fields) as $cat) {
+      if (!empty($menu_object->$cat)) {
+        if(!empty($menu_object->{$cat}[LANGUAGE_NONE][0]['option'])) {
+          //overriding category if the value was found
+          $category = $menu_object->{$cat}[LANGUAGE_NONE][0]['option'];
+          break;
+        }
+      }
+    }
   }
 }
