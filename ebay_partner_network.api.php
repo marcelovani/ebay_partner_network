dfa777c71d01b2908f5eef9d6e8fa42d0b8e6b39
Konstantin Kamskov <konstantin@kamskov.lt>
2014-12-10 16:57:25 +0000

    changing the example in the .api.php file

--- ebay_partner_network.api.php
+++ ebay_partner_network.api.php
@@ -22,17 +22,20 @@ function hook_ebay_partner_network_category_alter(&$category) {
   // getting all fields of the 'ebay_category' type
   $ebay_category_fields = ebay_partner_network_category_fields();
   if(sizeof($ebay_category_fields) > 0) {
-    //checking if ebay category is set on the node
+    //checking if ebay category is set on the primary category, if yes, then use it
     $menu_object = menu_get_object();
-    foreach(array_keys($ebay_category_fields) as $cat) {
-      if (!empty($menu_object->$cat)) {
-        if(!empty($menu_object->{$cat}[LANGUAGE_NONE][0]['option'])) {
-          //overriding category if the value was found
-          $category = $menu_object->{$cat}[LANGUAGE_NONE][0]['option'];
-          break;
+    if(!empty($menu_object->field_category_primary[LANGUAGE_NONE])) {
+        foreach($menu_object->field_category_primary[LANGUAGE_NONE] as $term) {
+          foreach(array_keys($ebay_category_fields) as $cat) {
+            if (!empty($term['taxonomy_term']->$cat)) {
+              if(!empty($term['taxonomy_term']->{$cat}[LANGUAGE_NONE][0]['option'])) {
+                $category = $term['taxonomy_term']->{$cat}[LANGUAGE_NONE][0]['option'];
+                break;
+              }
+            }
+          }
         }
-      }
-    }
+     }
   }
 }
 
